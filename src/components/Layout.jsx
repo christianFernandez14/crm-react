1e328ca6cc992329721e616fb4af715b93598424
Christian Fernandez <christian14@gmail.com>
2023-11-24 14:51:19 -0300

    Incorporando Link como performance a mis enlaces, gracias a react-router-dom

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,10 +1,25 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, Link } from "react-router-dom";
 
 const Layout = () => {
   return (
     <div className="md:flex md:min-h-screen">
       <aside className="md: w-1/4 bg-blue-600 px-5 py-10">
         <h2 className="text-4xl font-black text-center text-white">CRM - Clientes</h2>
+
+        <nav>
+          <Link
+            className="text-2xl block mt-2 hover:text-blue-300 text-white"
+            to="/"
+          >
+            Clientes
+          </Link>
+          <Link
+            className="text-2xl block mt-2 hover:text-blue-300 text-white"
+            to="/clientes/nuevo"
+          >
+            Cliente Nuevo
+          </Link>
+        </nav>
       </aside>
       <main className="md:w-3/4 p-10 md:h-screen overflow-scroll">
         <Outlet />
@@ -19,21 +34,15 @@ export default Layout
 
 
 
-//? Trabajando con el Componente Parte #1
+//? Incorporando Link
 /* 
-Este diseño tendra un barra lateral a la izquierda y a la derecha un contenido principal
-  1.- Comenzamos con algunas clases en el div principal, donde de doy un display flex (md:flex)
-      y total de altura  de la pantalla (md:min-h-screen)
-  2.- El segundo contenedor (aside), maneja una columna de 4 (md: w-1/4), con un fon azul, 
-      un padding
-      horizontal 20px y veritcal de 40px (la manera de saber cuantos px son, te posicionas con
-      el cursor en la clase y te da la referecnia del css que se le esta aplicando)
-  3.- El contenido princiapl (main), maneja el resto de las columnas disponibles, con padding 
-      en todos los lados 40px, toma el 100% de la pantalla, y esta parte esta habilitada el scroll
-      y dentro de el colocamos el Componente especial (Outlet), para que se vayan inyectando 
-      aquellos pages, que tenemos ya y la que diseñaremos.
-
-  4.-Le dimos un tiutlo principal con algunas clases que menajan un estilos css 
+  1.- Muy bien sabemos que para trabajar con enlances y navegación, el tag (<a href="http://"></a>)
+      anchor vendria bien, pero React, nos trae algo mas otimizado que es propio de
+      react-router-dom y este es es Link
+  2.- Para darle continudad al intem anterior, debes extarerlo 
+  3.- Lo incluidmos dentro de una nav (utlizando los tag correctos por temas SEO)
+  4.- Le agregamos algo de estilo ambos link
+  5.- Si le das a inspecionar a los link, veras que vendra con el tag (a), pero mas optimizado
 
 
 */
