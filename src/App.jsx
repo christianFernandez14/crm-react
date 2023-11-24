07f1e81ddd48e28d76e704b9587053fce601d6fb
Christian Fernandez <christian14@gmail.com>
2023-11-24 07:47:18 -0300

    Un poco de teoria: Que es el Routing

--- src/App.jsx
+++ src/App.jsx
@@ -13,23 +13,29 @@ function App() {
 
 export default App
 
-//? Pasos a seguir:
+//? Que es el Routing
+
+/* 
+1.- Hasta  el momento solo hemos trabajado con SPA, single page aplication
+2.- A traves de condicionales podias hacer la expreciencia de usuario de estabas en
+    otra pagina, quitando y mostrando componentes.
+3.- Routing (librerira de react), te permite trabajar con varias url, tener  y restringir 
+    rutas 
+4.- la que se usa es React-Roter / React-Router-DOM y en Version 6 las unieron y crearon a
+    Reach Router.
+5.- React-Router-DOM (RRD), como lo dijimos en el item anterior, libreria para crear app con Routing
+    (direfentes Urls)
+6.- Tendremos un mejor orden y por supuesto que podremos seguir re-utilizando los componentes que
+    vayamos creando
+7.- A partit de la V6.4 RRD, pasa a ser un framework, con manejo de rutas, peticiones http, 
+    formularios y datos
 
 
-//  1.- Ya creado el proyecto, instaladas las dependencas bases y arriba el proyecto (npm run dev)
-//  2.- Instalamos dependencias para trabajar con tailwind (npm i -d tailwindcss postcss autoprefixer)
-//      recuerda que la -d es porque la estamosinstalando bajo un ambiente de desarrollo
-//  3.- instalamos los archivos de configuración para que pueda trabajar tailwind (npx tailwindcss init -p)
-//  4.- Con el comando anterior se crean dos arhivos en el directorio raiz de tu proyecto 
-//      (postcss.config.js tailwind.config.js)
-//  5.- de los dos archivos que se crearon entramos en tailwind.config.js y configuramos la propiedad 
-//      "content" de esta manera content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}']
-//  6.- Vas a tu index.css, borras el contenido que exite ahi y colocalas lo siguiente
-//      @tailwind base;
-//      @tailwind components;
-//      @tailwind utilities;
-//  7.- Ya con esta configuracion puedes comnezar usar las predefinidas de tailwind en tu proyecto
-//  8.- Si por algun razon no ves los cambio, deten el servidor y vuelvelo a recargar.
+
+*/
+
+
+
 
 
 
