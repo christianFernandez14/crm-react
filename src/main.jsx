879ff7ed4ab25db8e034fbdb52795eb161fcc97b
Christian Fernandez <christian14@gmail.com>
2023-11-25 07:05:04 -0300

    Configurando el Loader - exportandolo - importandolo - y como se renombra

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Layout from './components/Layout';
 import NuevoCliente from './pages/NuevoCliente';
-import Index from './pages/Index';
+import Index, { Loader as clientesLoader } from './pages/Index';
 import './index.css'
 
 const router = createBrowserRouter([
@@ -13,7 +13,8 @@ const router = createBrowserRouter([
     children: [
       {
         index: true,
-        element: <Index />
+        element: <Index />,
+        loader: clientesLoader
       },
       {
         path: '/clientes/nuevo',
@@ -30,24 +31,15 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   </React.StrictMode>,
 )
 
-//? Pasos de tu estructura principal
+//? Imporntando el Loader
 
 /* 
-1.- Tu primero componente debe ser un Layout, por lo tanto vas a crear dentro src/components/Layout.jsx, ya que se repetitra en multuples componentes
-2.- Importamos el Layout.jsx n tu componente que lo rederiza, para este caso es justo el main.jsx
-3.- Se lo pasamos al la propiedad "element" de la ruta.
-4.- Ahora como podras ver tenemos una tercera propiedad "children", no es mas que un array de objetos con elementos que seran renderizados y a su vez tendran el layout dentro de su estructura.
-5.- Lo anterior esta diseñado para evitar repetir codigo.
-6.- Siendo este un proyecto un  CRM, modificamos la ruta "/nosotros" por "/clientes/nuevo"
-7.- Vamos a separar los componentes que se comportoran como rutas (estos estaran en un folder de nombre pages) y los componentes que estan dentro components, renderizaran algo en particular (y que sera reutilizable).
-8.- Importo NuevoCliente desde page, como elemento hijo, para que me pueda mostrar mi Layout
-9.- Lo Renderizo en la propiedad "element" del path: '/clientes/nuevo'.
-10.- Separo lo que es el Layout de su contenido y para eso, creo otro elmento, que sera el primer
-     indice de children, donde manejara las propiedades de ("index" "element") la primera le 
-     especifico con un boolenao que lo que venga en "element" se mostrara como contenido y
-     esta vendra de pages.
-11.- Siguiendo el orden del item anterior, importo el componente Index.jsx al main y lo coloco
-     como valor de "element", para que pueda renderizar contenido y lo separo de Layout en si.
+  1.- Lo importas aca, como un destructuring, ya que la exportacion de tipo nombrado
+  2.- Como el Loader va a manejar estados de multiples componentes y todos van pasar
+      primero por aca, para luego pasarlo  los componentes, en pocas palablar va manejar muchos
+      loaders, por lo tanto es recomendable en la importancion asignarle un nombre con ("as")
+  3.- Y ahora tu componete sabe que existe un loader, cuando le pasas una propiedad de nombre
+      loader y el valor que importantes con el nuevo nombre asignado
 
 */
 
--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,18 +1,39 @@
+export function Loader() {
+
+  return 'Desde el louder'
+}
+
 
 const Index = () => {
   return (
-    <div>Mi pagina de inicio</div>
+    <>
+      <h1 className="font-black text-4xl text-blue-900">Clientes</h1>
+      <p className="mt-3">Administra tus Clientes</p>
+    </>
+
   )
 }
 
 export default Index
 
 
-//? Creando el compoonente 
+//? Trabajando con el page y Loader
 
 /* 
-1.- Utlizas el snnipet de rafce, para que te cree tu componente con formato arrow function
-2.- Este componente sera el contenido que estara en en el path:"/"
+  1.- Creamos algunas etiquetas con algunos estilos y todo dentro de un fragment
+  2.- react-router-dom, trabaja con state propios, es decir no es necesario usar el useState
+  3.- Para esto debemo exportar un funcion que se llama "louder" y se recomienda que este
+      en el mismo componente
+  4.- Como estamos en el page que manejara los clientes usualmente usariamos un useState, que 
+      lo trairiamos de alguna Api, o algun archivo de prueba dentro de tu proyecto, y se mostraria
+      gracias al useEffect, sea una vez o si apunta alguna dependecia
+  5.- Desde la version 6.4 de react-router-dom, te permite trabajar con loader
+  6.- Es muy similar al useEffect, ya que es una funcion que se va a ejecutar cuando el componente
+      cargue, por lo tanto es ideal para cargar un state o consultar una API y mosrar el resultado 
+      en un componente
+  7.- El loader, siempre va retornar algo, ya que de lo contrario no funcionara
+  8.- Si te fijas bien , el loader lo estas EXPORTANDO, en el componente donde lo vas usar.
+  9.- Para eso debes ir al main.jsx
 
 
 */
